e9679bcf97f0cf60704be75a6fc6cf4d54eddcf2
LangaMAd <artykovh@gmail.com>
2024-02-07 11:55:50 +0600

    add forms & add tasks

--- food_shop/apps/accounts/admin.py
+++ food_shop/apps/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+# from products.admin import BasketAdmin
+from .models import EmailVerification, User
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'image']
+    # inlines = [BasketAdmin]
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ['code', 'user', 'expiration']
+    fields = ['code', 'user', 'expiration', 'created']
+    readonly_fields: list[str] = ['created']
+
+
---
+++ food_shop/apps/accounts/forms.py
@@ -0,0 +1,81 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
+
+from .models import EmailVerification, User
+from users.tasks import send_email_verification
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': "Enter the user's name",
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Enter password',
+    }))
+
+    class Meta:
+        model = User
+        fields: list[str] = ['username', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Enter the name',
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Enter the surname',
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': "Enter the user's name",
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Enter the email address',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Enter the password',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Confirm the password',
+    }))
+
+    class Meta:
+        model = User
+        fields: list[str] = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def save(self, commit: bool = True):
+        user = super().save(commit=True)
+        send_email_verification.delay(user.id)
+
+        return user
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+    }))
+    image = forms.ImageField(widget=forms.FileInput(attrs={
+        'class': 'custom-file-input',
+    }), required=False)
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'readonly': True
+    }))
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+    }))
+
+    class Meta:
+        model = User
+        fields: list[str] = ['first_name', 'last_name', 'image', 'username', 'email']
