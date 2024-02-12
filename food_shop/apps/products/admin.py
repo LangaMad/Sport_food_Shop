cb2cda9c25de6ddf179c514a02f5cd6c96c32849
Urmat <avoryxss@gmail.com>
2024-02-12 12:54:28 +0600

    add products_detail.html add product_list.html

--- food_shop/apps/products/admin.py
+++ food_shop/apps/products/admin.py
@@ -20,3 +20,5 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 
+
+
--- food_shop/apps/products/views.py
+++ food_shop/apps/products/views.py
@@ -7,8 +7,6 @@ from django.views.generic import TemplateView,ListView,DetailView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
-    queryset = Product.objects.all()
-    context_object_name = 'products'
 
 class ProductDetailView(DetailView):
     model = Product
@@ -16,21 +14,12 @@ class ProductDetailView(DetailView):
     template_name = 'product_detail.html'
     context_object_name = 'product'
 
-class CategoryListView(ListView):
-    model = Category
-    queryset = Category.objects.all()
-    template_name = 'category_list.html'
-    context_object_name = 'categories'
 
 
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.all()
+    template_name = 'product_list.html'
+    context_object_name = 'products'
 
-# class AboutView(TemplateView):
-#     template_name = 'about.html'
-#
-# class ShopView(TemplateView):
-#     template_name = 'shop.html'
-#
-# class ContactView(TemplateView):
-#     template_name = 'contact.html'
-#
 
