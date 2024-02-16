12360fea7d8c5b45bd6246f608a83e08b23b30b4
Urmat <avoryxss@gmail.com>
2024-02-16 12:03:05 +0600

    add serializers.py

--- food_shop/apps/products/api_urls.py
+++ food_shop/apps/products/api_urls.py
@@ -4,11 +4,15 @@ from .api_views import *
 
 urlpatterns = [
     path('product/', ProductListAPIView.as_view()),
-    path('product/<int:pk>/', ProductListAPIView.as_view()),
+    path('product/<int:pk>/', ProductRetrieveAPIView.as_view()),
     path('product/update/', ProductUpdateAPIView.as_view()),
     path('product/update/<int:pk>/', ProductUpdateAPIView.as_view()),
     path('category/', CategoryListAPIView.as_view()),
     path('category/<int:pk>/', CategoryListAPIView.as_view()),
     path('subcategory/', SubCategoryListAPIView.as_view()),
     path('subcategory/<int:pk>/', SubCategoryListAPIView.as_view()),
+    path('product/create/',ProductCreateAPIView.as_view()),
+    path('product/delete/<int:pk>/', ProductDestroyAPIView.as_view()),
+    path('subcategory/create/', SubCategoryCreateAPIView.as_view()),
+    path('subcategory/delete/<int:pk>/', SubCategoryDestroyAPIView.as_view()),
     ]
