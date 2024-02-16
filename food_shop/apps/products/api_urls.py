9048b77505df78a8a229bdb74c9e482f927e5076
Urmat <avoryxss@gmail.com>
2024-02-16 13:13:34 +0600

    add serializers.py

--- food_shop/apps/products/api_urls.py
+++ food_shop/apps/products/api_urls.py
@@ -2,17 +2,31 @@ from django.urls import path,include
 from .api_views import *
 
 
+
 urlpatterns = [
-    path('product/', ProductListAPIView.as_view()),
-    path('product/<int:pk>/', ProductRetrieveAPIView.as_view()),
-    path('product/update/', ProductUpdateAPIView.as_view()),
-    path('product/update/<int:pk>/', ProductUpdateAPIView.as_view()),
-    path('category/', CategoryListAPIView.as_view()),
-    path('category/<int:pk>/', CategoryListAPIView.as_view()),
-    path('subcategory/', SubCategoryListAPIView.as_view()),
-    path('subcategory/<int:pk>/', SubCategoryListAPIView.as_view()),
-    path('product/create/',ProductCreateAPIView.as_view()),
-    path('product/delete/<int:pk>/', ProductDestroyAPIView.as_view()),
-    path('subcategory/create/', SubCategoryCreateAPIView.as_view()),
-    path('subcategory/delete/<int:pk>/', SubCategoryDestroyAPIView.as_view()),
+    path('product/', ProductListCreateAPIView.as_view()),
+    path('product/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view()),
+    path('product/<int:pk>/', ProductRetrieveDestroyAPIView.as_view()),
+    path('product/<int:pk>/', ProductRetrieveCreateAPIView.as_view()),
+    path('product/<int:pk>/', ProductRetriveDestroyUpdateAPIView.as_view()),
     ]
+
+
+
+
+
+
+# urlpatterns = [
+#     path('product/', ProductListAPIView.as_view()),
+#     path('product/<int:pk>/', ProductRetrieveAPIView.as_view()),
+#     path('product/update/', ProductUpdateAPIView.as_view()),
+#     path('product/update/<int:pk>/', ProductUpdateAPIView.as_view()),
+#     path('category/', CategoryListAPIView.as_view()),
+#     path('category/<int:pk>/', CategoryListAPIView.as_view()),
+#     path('subcategory/', SubCategoryListAPIView.as_view()),
+#     path('subcategory/<int:pk>/', SubCategoryListAPIView.as_view()),
+#     path('product/create/',ProductCreateAPIView.as_view()),
+#     path('product/delete/<int:pk>/', ProductDestroyAPIView.as_view()),
+#     path('subcategory/create/', SubCategoryCreateAPIView.as_view()),
+#     path('subcategory/delete/<int:pk>/', SubCategoryDestroyAPIView.as_view()),
+#     ]
